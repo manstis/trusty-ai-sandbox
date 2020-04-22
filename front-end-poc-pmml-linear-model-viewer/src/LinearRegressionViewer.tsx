fe9892872fa8a00f8911679f7898d981a5eaa311
Michael Anstis <manstis@redhat.com>
2020-04-22 21:26:17 +0100

    Refactor LinearRegressionViewer to useState(..) for the component

--- front-end-poc-pmml-linear-model-viewer/src/LinearRegressionViewer.tsx
+++ front-end-poc-pmml-linear-model-viewer/src/LinearRegressionViewer.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { document as PMMLDocument } from "./generated/www.dmg.org/PMML-4_4";
 import { LinearRegressionViewAdaptor } from "./LinearRegressionViewAdaptor";
 import { withCXML as unmarshal } from "./unmarshall/unmarshaller";
+import { Spinner } from '@patternfly/react-core';
 
 type Props = {
   xml: string
@@ -10,22 +11,17 @@ type Props = {
 
 const LinearRegressionViewer = (props: Props) => {
 
+  const [viewer, setViewer] = useState(<><Spinner size="xl" /></>);
+
   unmarshal(props.xml).then((doc: PMMLDocument) => {
     if (doc.PMML.RegressionModel !== undefined) {
       if (doc.PMML.RegressionModel[0] !== undefined) {
-        ReactDOM.render(
-          <React.StrictMode>
-            <LinearRegressionViewAdaptor dictionary={doc.PMML.DataDictionary} model={doc.PMML.RegressionModel} />
-          </React.StrictMode>,
-          document.getElementById("holder")
-        );
+        setViewer(<><LinearRegressionViewAdaptor dictionary={doc.PMML.DataDictionary} model={doc.PMML.RegressionModel} /></>);
       }
     };
   });
 
-  return (
-    <div id="holder" />
-  );
+  return viewer;
 }
 
 export { LinearRegressionViewer };
